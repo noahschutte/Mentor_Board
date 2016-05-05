efb2977c326e2a43eb451b62a6c8f000787417d5
Jon Schwartz & Noah Schutte & Theo Paul <pair+jonschwartz93.noahschutte.theomarkkuspaul@devbootcamp.com>
2016-05-05 16:59:52 -0400

    Add date and time to appointment creation

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,7 +1,13 @@
 class AppointmentsController < ApplicationController
 
   def index
-    @appointments = Appointment.all
+    if current_user.type == "Mentor"
+      @appointments = Appointment.where(mentor_id: current_user.id)
+    elsif
+      @appointments = Appointment.all
+    else
+      redirect_to root_path
+    end
   end
 
   def show
@@ -10,19 +16,38 @@ class AppointmentsController < ApplicationController
     @student = @appointment.student
     @mentor = @appointment.mentor
     @reviews = @appointment.reviews
-    @type = @reviews.first.author.type
+    if @reviews.first
+      @type = @reviews.first.author.type
+    elsif @reviews.last
+      @type = @reviews.last.author.type
+    end
   end
 
   def new
     @appointment = Appointment.new
+    @skills = Skill.all
   end
 
   def create
-    @appointment = Appointment.new(appointment_params)
+    @time_format = params[:appointment]["time(4i)"].to_s + ":" + params[:appointment]["time(5i)"].to_s
+    @appointment = Appointment.new(date: params[:appointment][:date], time: @time_format, mentor_id: current_user.id)
+    params[:appointment][:skills].select do |skill, value|
+      if value == "1"
+        @appointment.skills << Skill.where(name: skill)
+      end
+    end
+    if @appointment.skills.empty?
+      render 'new'
+    end
+    if @appointment.save
+      redirect_to appointments_path
+    else
+      render 'new'
+    end
   end
 
   private
   def appointment_params
-    params.require(:appointment).permit(:date, :time)
+    params.require(:appointment).permit(:date, :time, :mentor_id)
   end
 end
--- app/models/appointment_skill.rb
+++ app/models/appointment_skill.rb
@@ -1,5 +1,5 @@
 class AppointmentSkill < ActiveRecord::Base
-  validates :appointment_id, :skill_id, presence: true
+  validates :skill_id, presence: true
 
   belongs_to :appointment
   belongs_to :skill
