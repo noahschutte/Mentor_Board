7d98391ad9cba36fcceea8528ae33f4521292231
Noah Schutte <noahschutte@gmail.com>
2016-05-06 18:21:25 -0400

    bundle install imageuploader merge

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160505161420) do
+ActiveRecord::Schema.define(version: 20160506200624) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -59,6 +59,7 @@ ActiveRecord::Schema.define(version: 20160505161420) do
     t.string   "type",            null: false
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.string   "image"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", using: :btree
